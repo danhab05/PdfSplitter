a196c1a5891331e69ee676d97f234bc85a24f457
Dan Habib <danhabib011@gmail.com>
2025-10-03 09:12:13 +0000

    jai ca google.com, pub-1380656527637231, DIRECT, f08c47fec0942fa0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"></link>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1380656527637231"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
@@ -34,5 +36,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-    
